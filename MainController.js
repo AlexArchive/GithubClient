76344367d69c19d964037b7eb98ffc8df328c72a
ByteBlast <byteblast@gmail.com>
2014-11-07 11:33:04 +0000

    Added simple profile information view.

--- MainController.js
+++ MainController.js
@@ -1,2 +1,9 @@
-app.controller("MainController", function ($scope) {
+app.controller("MainController", function ($scope, $http) {
+    
+    var onUserAvailable = function(response) {
+        $scope.user = response.data;
+    }
+    
+    $http.get("https://api.github.com/users/byteblast").then(onUserAvailable);
+    
 });
