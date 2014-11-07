7a5f5dd167d37acf52b3504e3e011784ff45a3a3
ByteBlast <byteblast@gmail.com>
2014-11-07 15:28:08 +0000

    Applied code formatting tool.

--- MainController.js
+++ MainController.js
@@ -1,20 +1,20 @@
-app.controller("mainController", function ($scope, $http) {
-    
+app.controller("mainController", function($scope, $http) {
+
     function onUserAvailable(response) {
         $scope.user = response.data;
         $http.get($scope.user.repos_url).then(onReposAvailable, onError)
     };
-    
+
     function onReposAvailable(response) {
         $scope.repos = response.data;
     }
-    
+
     function onError(reason) {
-        $scope.error = "Could not fetch data.";  
+        $scope.error = "Could not fetch data.";
     };
-    
+
     $scope.search = function(username) {
         $http.get("https://api.github.com/users/" + username).then(onUserAvailable, onError);
     }
-    
+
 });
