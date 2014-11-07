7a642521f02809c50337ba05a78385f0d90b5aa4
ByteBlast <byteblast@gmail.com>
2014-11-07 15:25:43 +0000

    Added user repos to view.

--- MainController.js
+++ MainController.js
@@ -2,10 +2,15 @@ app.controller("mainController", function ($scope, $http) {
     
     function onUserAvailable(response) {
         $scope.user = response.data;
+        $http.get($scope.user.repos_url).then(onReposAvailable, onError)
     };
     
+    function onReposAvailable(response) {
+        $scope.repos = response.data;
+    }
+    
     function onError(reason) {
-        $scope.error = "Could not fetch user at this time.";  
+        $scope.error = "Could not fetch data.";  
     };
     
     $scope.search = function(username) {
